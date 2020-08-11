ce5a8f5cf20d02eeb1fbf933a7e4010c8e1040b0
Jonathan Guichard <jona.guich69@gmail.com>
2020-08-11 23:25:43 +0200

    Add docker-compose
    arm64

--- docker/arm/arm64.sh
+++ docker/arm/arm64.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 host="10.0.50.24"
 user="ubuntu"
-
+app_name="external-server"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 
@@ -21,5 +21,10 @@ scp ../../back/package.json $user@$host:~/docker-building/back/package.json
 
 scp ../DockerFile $user@$host:~/docker-building/DockerFile
 
-ssh $user@$host "cd ~/docker-building && docker build -f ./DockerFile . -t elyspio/external-server:arm64"
-ssh $user@$host "docker push elyspio/external-server:arm64"
+ssh $user@$host "cd ~/docker-building && docker build -f ./DockerFile . -t elyspio/$app_name:arm64"
+ssh $user@$host "docker push elyspio/$app_name:arm64"
+
+
+scp ./docker-compose.yml $user@$host:/apps/$app_name/docker-compose.yml
+ssh $user@$host "cd /apps/$app_name/ && docker-compose up -d"
+
