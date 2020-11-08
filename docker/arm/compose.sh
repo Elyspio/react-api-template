01997d59a46ade99471c9b5067acd5a6b66c3221
Jonathan Guichard <jona.guich69@gmail.com>
2020-11-08 19:15:59 +0100

    Back: update package.json's script

--- docker/arm/compose.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-. "$DIR/variables.sh"
-
-ssh $sshConnection "mkdir -p /apps/$app"
-scp "$DIR/docker-compose.yml" "$sshConnection:/apps/$app/docker-compose.yml"
-ssh $sshConnection "cd /apps/$app && docker-compose up -d"
